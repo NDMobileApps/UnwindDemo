1d247096a31d66326de8a3573c03312989ea85d6
Brandon Rich <brich@nd.edu1>
2016-02-29 18:36:49 -0500

    initial revision

---
+++ UnwindDemo/ThreeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThreeViewController.swift
+//  UnwindDemo
+//
+//  Created by Brandon Rich2 on 2/17/16.
+//  Copyright © 2016 Infinite Donuts. All rights reserved.
+//
+
+import UIKit
+
+class ThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        print("preparing for segue")
+    }
+
+}
--- UnwindDemo/ViewController.swift
+++ UnwindDemo/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var alert = UIAlertController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        alert = UIAlertController (title: "Foo!", message: "bar", preferredStyle: .Alert )
+        alert.addAction( UIAlertAction(
+            title: "thing 1",
+            style: .Default ) {
+                (action:UIAlertAction) -> Void in
+                   print("thing 1 clicked")
+            }
+        )
+
+        alert.addAction( UIAlertAction(
+            title: "thing 1",
+            style: .Destructive ) {
+                (action:UIAlertAction) -> Void in
+                print("thing 2 clicked")
+            }
+        )
+        alert.addAction( UIAlertAction(
+            title: "Cancel",
+            style: .Cancel ) {
+                (action:UIAlertAction) -> Void in
+                print("cancel clicked")
+            }
+        )
+        
+        alert.addAction( UIAlertAction(
+            title: "get text",
+            style: .Default ) {
+                (action:UIAlertAction) -> Void in
+                let tf = self.alert.textFields?.first
+                print(tf!.text!)
+                
+            }
+        )
+        alert.addTextFieldWithConfigurationHandler() {
+            (textField) in
+            textField.placeholder = "Foo"
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwindToPageOne(segue: UIStoryboardSegue) {
+        
+    }
 
+    @IBAction func actionSheetButtonClicked(sender: AnyObject) {
+        presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
